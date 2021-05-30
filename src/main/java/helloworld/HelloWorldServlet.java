d80a7ed7cbf3a781e2ba5744e0d7191a3e992aa1
Manfred Riem <22894110+manorrock@users.noreply.github.com>
2021-05-30 11:44:14 -0600

    Fixes issue #6 - Repurpose piranha-nano-helloworld

--- src/main/java/helloworld/HelloWorldServlet.java
+++ src/main/java/helloworld/HelloWorldServlet.java
@@ -11,8 +11,22 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+/**
+ * This Servlet illustrates how to create a 'Hello World' server using Piranha
+ * Nano.
+ *
+ * @author Manfred Riem (mriem@manorrock.com)
+ */
 public class HelloWorldServlet extends HttpServlet {
 
+    /**
+     * Handle the GET requests.
+     *
+     * @param request the request.
+     * @param response the response.
+     * @throws IOException when an I/O error occurs.
+     * @throws ServletException when a Servlet error occurs.
+     */
     @Override
     protected void doGet(HttpServletRequest request, 
             HttpServletResponse response) throws IOException, ServletException {
@@ -26,10 +40,10 @@ public class HelloWorldServlet extends HttpServlet {
      * Main method.
      * 
      * <p>
-     *  The FUNCTIONS_CUSTOMHANDLER_PORT is the environment variable that
-     *  defines the port number the Azure Functions runtime wants this server
-     *  to run on. When the environment variable is not present the server will
-     *  run on port 8080.
+     * The FUNCTIONS_CUSTOMHANDLER_PORT is the environment variable that defines
+     * the port number the Azure Functions runtime wants this server to run on.
+     * When the environment variable is not present the server will run on port
+     * 8080.
      * </p>
      * 
      * @param arguments the command-line arguments.
@@ -39,10 +53,13 @@ public class HelloWorldServlet extends HttpServlet {
         NanoPiranha piranha = new NanoPiranhaBuilder()
                 .servlet("HelloWorld", new HelloWorldServlet())
                 .build();
+        
         int port = System.getenv("FUNCTIONS_CUSTOMHANDLER_PORT") != null 
                 ? Integer.parseInt(System.getenv("FUNCTIONS_CUSTOMHANDLER_PORT")) : 8080;
+        
         DefaultHttpServer server = new DefaultHttpServer(port, 
                 new NanoHttpServerProcessor(piranha), false);
+        
         server.start();
     }
 }
